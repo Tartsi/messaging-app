94636b15d854ae3696690fc5d62cf02b7a9b4867
Tartsi <tarvo.lilja@hotmail.fi>
2023-10-05 03:07:02 +0300

    Setup .gitignore and Database

---
+++ database_setup.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# Define the database file name here
+# The database file will be created upon execution of this setup file
+DATABASE_NAME = 'database.db'
+
+# Placeholder
+
+
+def create_database():
+
+    try:
+
+        conn = sqlite3.connect(DATABASE_NAME)
+        cursor = conn.cursor()
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                          id INTEGER PRIMARY KEY,
+                          username TEXT,
+                          password TEXT)''')
+
+        conn.commit()
+        conn.close()
+        print(f"Database '{DATABASE_NAME}' created successfully.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    create_database()
