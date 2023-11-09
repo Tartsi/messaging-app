5de5a26f190f23eac1b3a429f311fd1af819b494
Tartsi <tarvo.lilja@hotmail.fi>
2023-11-09 03:11:31 +0200

    Typo fix

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = "secret_key1234"
-# SECURITY NOTIFICATION: The key should be more way more random.
+# SECURITY NOTIFICATION: The key should be more random.
 # In this form this is more of a "theoretical" and not a "realistic" security flaw.
