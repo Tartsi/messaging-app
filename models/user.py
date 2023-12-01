8ed22ef5b31c70fc4ec2538e27cd73f74debd324
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2023-12-01 19:46:10 +0200

    Update models

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 class User:
 
-    def __init__(self, username, password, admin_status=0):
+    def __init__(self, username, password, admin_status=False):
         self.username = username
         self.password = password
         self.admin_status = admin_status
 
     def __str__(self):
 
-        if (self.admin_status == 1):
+        if (self.admin_status == True):
             return "User {self.username} is an admin."
 
         return "User {self.username} is not an admin."
